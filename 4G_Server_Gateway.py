483fedf6b6a6a5268420f76698919aa55c1f3ac4
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-12-18 13:44:52 +0600

    modified

--- 4G_Server_Gateway.py
+++ 4G_Server_Gateway.py
@@ -19,11 +19,11 @@ import time
 #import do200
 
 import df703
-import df555
-import df556
-import dc400
-import dc413
-import df572
+# import df555
+# import df556
+# import dc400
+# import dc413
+# import df572
 #import dt310
 
 
